8841b788aceecd57bab37b90c64b6e5fc613a6f1
nvjib <nvjibiman@gmail.com>
2025-09-11 15:40:52 +0100

    feat: add login with JWT

--- server.js
+++ server.js
@@ -38,5 +38,27 @@ app.post("/sign-up", async (req, res) => {
     return res.status(200).json({ message: "User created successfully", token })
 })
 
+// login 
+app.post("/login", async (req, res) => {
+    const { email, password } = req.body
+
+    if (!email || !password) return res.status(400).json({ error: "Missing required fields" })
+
+    const { data: existingUser } = await supabase
+        .from("users")
+        .select("*")
+        .eq("email", email)
+        .single()
+
+    if (!existingUser) return res.status(404).json({ error: "User not found" })
+
+    const isValidPassword = await bcrypt.compare(password, existingUser.password)
+    if (!isValidPassword) return res.status(500).json({ error: "Invalid password" })
+
+    const token = jwt.sign({ email }, JWT_SECRET, { expiresIn: "1h" })  
+
+    return res.status(200).json({ message: "Logged in successfully", token })
+})
+
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => console.log(`Server is running on: http://localhost:${PORT}`))
