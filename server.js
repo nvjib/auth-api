eafc88e291368bff01e557774daa691e1a3ebbcd
nvjib <nvjibiman@gmail.com>
2025-09-14 23:56:22 +0100

    fix: test

--- server.js
+++ server.js
@@ -8,5 +8,5 @@ app.use(express.json())
 app.use(cors())
 app.use("/", auth)
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 30000
 app.listen(PORT, () => console.log(`Server is running on: http://localhost:${PORT}`))
